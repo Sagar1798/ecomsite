9bba9947d9e8052c4dfb46975bca44d20834d3b5
Sagar1798 <sagarkr1798@gmail.com>
2020-02-28 01:07:42 +0530

    first ecomsite

---
+++ shop/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from .models import Products,Order
+
+
+# Register your models here.
+admin.site.site_header = "E-commerce-Site"
+
+admin.site.site_tittle = "SKS Shopping"
+admin.site.index_title="Manage SKS Shopping"
+
+class ProductAdmin(admin.ModelAdmin):
+
+    def change_category_to_default(self,request,queryset):
+        queryset.update(category='default')
+
+    change_category_to_default.short_description = 'Default Category'
+    list_display = ('title','price','discount_price','category','description','image')
+    search_fields = ('title',)
+    actions = ('change_category_to_default',)
+    fields = ('title','price',)
+    list_editable = ('price','category',)
+
+admin.site.register(Products,ProductAdmin)
+admin.site.register(Order)
